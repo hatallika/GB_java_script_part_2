5da9115d5bb29bd5f83a875b784dffdbbc2d3765
Deev Maksim (DevMArt) <deev.maks2012@ya.ru>
2021-03-05 21:37:40 +0500

    lesson2

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
+class ApiMock {
+    constructor() {
 
-const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
-  
-const renderGoodsList = (list = goods) => {
-    let goodsList = list.map(
-            item => renderGoodsItem(item)
-        ).join('\n');
+    }
 
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+    fetch() {
+      return [
+          { title: 'Shirt', price: 150 },
+          { title: 'Socks', price: 50 },
+          { title: 'Jacket', price: 350 },
+          { title: 'Shoes', price: 250 },
+        ];
+    }
 }
-  
-renderGoodsList();
+
+class GoodsItem {
+    constructor(title, price) {
+      this.title = title;
+      this.price = price;
+    }
+
+    getHtml() {
+      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+      this.api = new ApiMock();
+      this.$goodsList = document.querySelector('.goods-list');
+      this.goods = [];
+    }
+
+    fetchGoods() {
+      this.goods = this.api.fetch().map(({title, price}) => new GoodsItem(title, price));
+    }
+
+    render() {
+      this.$goodsList.textContent = '';
+      this.goods.forEach((good) => {
+          this.$goodsList.insertAdjacentHTML('beforeend', good.getHtml());
+      })
+    }
+}
+
+const goodsList = new GoodsList();
+
+goodsList.fetchGoods();
+goodsList.render();
